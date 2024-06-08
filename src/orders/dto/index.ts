be5b163ace6342a2e0571a8a46515e2cf79de5d8
Wilmer Guedez <148584989+wguedex@users.noreply.github.com>
2024-06-07 23:22:43 -0500

    preparar base de datos y PaidOrderDto

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -3,3 +3,4 @@ export { CreateOrderDto } from "./create-order.dto";
 export { UpdateOrderDto } from "./update-order.dto"; 
 export { ChangeOrderStatusDTO } from "./change-order-status.dto";
 export { OrderItemDTO } from './order-item.dto';
+export { PaidOrderDto } from "./paid-order.dto";
